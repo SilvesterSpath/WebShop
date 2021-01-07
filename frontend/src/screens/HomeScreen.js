76168cdb0ab1adfa347e02ffe320d0ed0763a98f
SilvesterSpath <spath.sz@gmail.com>
2021-01-07 14:51:35 +0100

    stars

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import products from '../products'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
+import React from 'react';
+import products from '../products';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 
-const HomeScreens = () => { 
-    return (
-        <>
-           <h1>Latest Products</h1>
-           <Row>
-               {products.map(i =>(
-                   <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={i}/>
-                   </Col>
-               ))}
-            </Row> 
-        </>
-    )
-}
+const HomeScreens = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((i) => (
+          <Col key={i._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={i} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreens
+export default HomeScreens;
