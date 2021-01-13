9a79fdfce5ada33b4fe2da1ca172fcd1b2ff7853
SilvesterSpath <spath.sz@gmail.com>
2021-01-13 09:35:28 +0100

    model the data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js ';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -22,4 +26,7 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
-app.listen(PORT, console.log(`Server running in ${NODE_ENV} on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${NODE_ENV} on port ${PORT}`.yellow.bold)
+);
