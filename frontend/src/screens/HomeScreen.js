237719632ad973d9b44f0d5208d56c14ce7bc25e
SilvesterSpath <spath.sz@gmail.com>
2021-01-18 08:52:10 +0100

    Redux state

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreens = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((i) => i.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+    dispatch(listProducts());
+  }, [dispatch]);
 
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  // const products = [];
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((i) => (
-          <Col key={i._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={i} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((i) => (
+            <Col key={i._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={i} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
