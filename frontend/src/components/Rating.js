3dac03fd2cf1f94b1ba6588684f128eae118ea7c
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-03-22 08:35:12 +0100

    Product review frontend

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -72,13 +72,13 @@ const Rating = ({ value, text, color }) => {
 Rating.defaultProps = {
   //color: '#f8e825',
   //color: '#1f4780',
-	color: '#008cff'
+  color: '#008cff',
 };
 
-Rating.propTypes = {
+/*Rating.propTypes = {
   value: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   color: PropTypes.string,
-};
+};*/
 
 export default Rating;
