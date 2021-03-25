93e73e42c9299894862bf25240e2e762e1ae476b
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-03-25 09:40:33 +0100

    Paginate

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,18 +4,22 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreens = ({ match }) => {
   const keyword = match.params.keyword;
+
+  const pageNumber = match.params.pageNumber || 1;
+
   const dispatch = useDispatch();
 
   const productList = useSelector((i) => i.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, page, pages } = productList;
 
   useEffect(() => {
-    dispatch(listProducts(keyword));
-  }, [dispatch, keyword]);
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
   // const products = [];
 
@@ -27,13 +31,20 @@ const HomeScreens = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((i) => (
-            <Col key={i._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={i} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((i) => (
+              <Col key={i._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={i} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </>
   );
