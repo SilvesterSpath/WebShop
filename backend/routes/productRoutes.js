86a7a9eaad5e713897554f9628013bf440747a97
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-03-26 07:52:37 +0100

    Carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -14,6 +15,7 @@ const router = express.Router();
 //router.get('/', getProducts);
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
+router.get('/top', getTopProducts);
 
 //router.get('/:id', getProductById);
 router
---
+++ frontend/src/components/ProductCarousel.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Loader from './Loader';
+import Message from './Message';
+import { listTopProducts } from '../actions/productActions';
+
+const ProductCarousel = () => {
+  const dispatch = useDispatch();
+
+  const productTopRated = useSelector((i) => i.productTopRated);
+  const { loading, error, products } = productTopRated;
+
+  useEffect(() => {
+    dispatch(listTopProducts());
+  }, [dispatch]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <Carousel pause='hover' className='bg-dark'>
+      {products.map((i) => (
+        <Carousel.Item key={i._id}>
+          <Link to={`/product/${i._id}`}>
+            <Image src={i.image} alt={i.name} fluid />
+            <Carousel.Caption className='carousel-caption'>
+              <h2>
+                {i.name} ({i.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ProductCarousel;
