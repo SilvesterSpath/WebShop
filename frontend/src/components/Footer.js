e3ddda0441e8bbbe3716462190996ad0da647ac2
SilvesterSpath <spath.sz@gmail.com>
2026-04-28 06:25:26 +0200

    design improvements

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-  
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className='text-center py-3'>Copyright &copy; WebShop 2021</Col>
-                </Row>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; WebShop 2025</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
